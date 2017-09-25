8faacb9a1b24e894bc60e70d50c06d8db5bba553
user2 <4708_37616@noemail.com>
2017-09-25 17:59:24 +0530

    first day

---
+++ src/main/java/com/model/Passenger.java
@@ -0,0 +1,110 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the passenger database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Passenger.findAll", query="SELECT p FROM Passenger p")
+public class Passenger implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="PASSENGER_ID")
+	private int passengerId;
+
+	@Column(name="FIRST_NAME")
+	private String firstName;
+
+	@Column(name="LAST_NAME")
+	private String lastName;
+
+	@Column(name="MEAL_PREFERENCES")
+	private String mealPreferences;
+
+	@Column(name="MIDDLE_NAME")
+	private String middleName;
+
+	@Column(name="passenger_type")
+	private String passengerType;
+
+	@Column(name="PASSPORT_NUMBER")
+	private String passportNumber;
+
+	//bi-directional many-to-one association to Booking
+	@ManyToOne
+	@JoinColumn(name="BOOKING_ID")
+	private Booking booking;
+
+	public Passenger() {
+	}
+
+	public int getPassengerId() {
+		return this.passengerId;
+	}
+
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMealPreferences() {
+		return this.mealPreferences;
+	}
+
+	public void setMealPreferences(String mealPreferences) {
+		this.mealPreferences = mealPreferences;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getPassengerType() {
+		return this.passengerType;
+	}
+
+	public void setPassengerType(String passengerType) {
+		this.passengerType = passengerType;
+	}
+
+	public String getPassportNumber() {
+		return this.passportNumber;
+	}
+
+	public void setPassportNumber(String passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+	public Booking getBooking() {
+		return this.booking;
+	}
+
+	public void setBooking(Booking booking) {
+		this.booking = booking;
+	}
+
+}
