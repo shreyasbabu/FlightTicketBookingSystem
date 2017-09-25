0f940b2bff06b52a8d4a604fe54b29651dc9c123
4708_37615 <4708_37615@noemail.com>
2017-09-25 18:22:46 +0530

    first day

---
+++ src/main/java/com/dao/FleetMasterDAO.java
@@ -0,0 +1,14 @@
+package main.java.com.dao;
+
+import java.util.List;
+
+import model.FleetMaster;
+
+public interface FleetMasterDAO {
+	
+	public void createFleet(FleetMaster fleetMaster);
+	public void modifyFleet(FleetMaster fleetMaster);
+	public void deleteFleet(int fleetId);
+	public FleetMaster getFleet(int fleetId);
+	public List<FleetMaster> getAllFleet();
+}
