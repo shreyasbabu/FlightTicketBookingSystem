fca61a688884659b61e15a138fa0ce4e3886cbc4
pandilwar605 <pandilwar605@gmail.com>
2017-09-26 22:15:17 +0530

    Service layer implementation

---
+++ src/main/java/com/service/UserServiceImplentation.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.util.List;
+
+public class UserServiceImplentation implements UserService {
+
+	@Override
+	public boolean checkLogin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkIfUserExists(String emailId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean verifyEmail(String emailId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List getHistory(int personId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancelBooking(int bookingId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
